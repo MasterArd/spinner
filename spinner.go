f9b14be81ed627f867c91f746192d26312fa4341
MasterArd <arda.uk12@gmail.com>
2026-06-30 22:30:51 +0200

    Refactor spinner layout and animation logic

--- spinner.go
+++ spinner.go
@@ -10,19 +10,44 @@ import (
 	"fyne.io/fyne/v2/container"
 )
 
-func New() fyne.CanvasObject {
-	const (
-		dots   = 8
-		radius = 20
-		size   = 8
-	)
+const (
+	dots   = 8
+	radius = 20
+	size   = 8
+)
 
+type spinnerLayout struct{}
+
+func (d *spinnerLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
+	diameter := float32((radius + size) * 2)
+	return fyne.NewSize(diameter, diameter)
+}
+
+func (d *spinnerLayout) Layout(objects []fyne.CanvasObject, containerSize fyne.Size) {
+	if len(objects) == 0 {
+		return
+	}
+
+	centerX := containerSize.Width / 2
+	centerY := containerSize.Height / 2
+
+	for i, obj := range objects {
+		angle := (float64(i) / float64(len(objects))) * 2 * math.Pi
+
+		x := centerX + float32(math.Cos(angle)*radius) - (size / 2)
+		y := centerY + float32(math.Sin(angle)*radius) - (size / 2)
+
+		obj.Move(fyne.NewPos(x, y))
+		obj.Resize(fyne.NewSize(size, size))
+	}
+}
+
+func New() fyne.CanvasObject {
 	circles := make([]*canvas.Circle, dots)
 	objects := make([]fyne.CanvasObject, dots)
 
 	for i := range dots {
 		c := canvas.NewCircle(color.NRGBA{255, 255, 255, 50})
-		c.Resize(fyne.NewSize(size, size))
 		circles[i] = c
 		objects[i] = c
 	}
@@ -34,13 +59,6 @@ func New() fyne.CanvasObject {
 
 		for range ticker.C {
 			for i := 0; i < dots; i++ {
-				angle := (float64(i) / dots) * 2 * math.Pi
-
-				x := float32(math.Cos(angle)*radius + radius + size)
-				y := float32(math.Sin(angle)*radius + radius + size)
-
-				circles[i].Move(fyne.NewPos(x, y))
-
 				if i == phase {
 					circles[i].FillColor = color.White
 				} else {
@@ -52,14 +70,11 @@ func New() fyne.CanvasObject {
 						A: alpha,
 					}
 				}
-				fyne.Do(func() {
-					circles[i].Refresh()
-				})
+				circles[i].Refresh()
 			}
-
 			phase = (phase + 1) % dots
 		}
 	}()
 
-	return container.NewWithoutLayout(objects...)
+	return container.New(&spinnerLayout{}, objects...)
 }
